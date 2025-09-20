2d299c4324dd587896a3174b3183a452387b52de
fspruce <f.spruce@proton.me>
2025-09-20 19:35:09 +0100

    Move autocomplete view to views file, remove autocomplete.py

--- store/views.py
+++ store/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from dal_select2.views import Select2QuerySetView
+from .models import Tag
 
 # Create your views here.
 
 
 def home_page_view(request):
     return HttpResponse("<h1>Hello, World!</h1>")
+
+
+class TagAutocomplete(Select2QuerySetView):
+    def get_queryset(self):
+        # Only allow authenticated users to use the autocomplete
+        if not self.request.user.is_authenticated:
+            return Tag.objects.none()
+
+        qs = Tag.objects.all()
+
+        if self.q:
+            # Filter the queryset bvased o nthe search term 'self.q'
+            qs = qs.filter(name__icontains=self.q)
+
+        return qs
