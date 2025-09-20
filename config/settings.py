1a6f4e893b38340c11a3866f3d786ac17def330d
fspruce <f.spruce@proton.me>
2025-09-20 17:40:02 +0100

    Add site_id to settings

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Site ID for django.contrib.sites
+SITE_ID = 1
